1115d56ce210c1e43c645dff4b40da8f29be20f4
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-13 22:03:08 +0530

    Update nr.c

--- nr.c
+++ nr.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
+
 #include <math.h>
-#define tol  0.001
-double f(double x){
-  
-	
-    return (x*x*x)-x-1;
+
+#define tol 0.000001
+double f(double x) {
+
+  return (x * x * x) - x - 1;
 }
-double f_x(double x){
-	
-	
-	
-    return (3*x*x-1);
+
+double f_x(double x) {
+
+  return (3 * x * x - 1);
 }
-double newtonraphson(double x){
-    double h = f(x) / f_x(x); 
-    while (h >= tol || -h>= tol) 
-    { 
-        h = f(x)/f_x(x); 
-        x = x - h;
-    }
-    printf("The value of the root is: %lf\n", x);
-    return x;
+
+void newtonraphson(double x) {
+  double h = f(x) / f_x(x);
+  while (h >= tol || -h >= tol) {
+    h = f(x) / f_x(x);
+    x = x - h;
+  }
+  printf("The value of the root is: %lf\n", x);
 }
-int main()
-{
-  
-        newtonraphson(2);
-   
-    return 0;
+
+void main() {
+
+  newtonraphson(2);
+
 }
